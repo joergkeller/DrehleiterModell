3186ddea6f9854a423d4b057d15592adbc29b61d
Jörg Keller <joergkeller@users.noreply.github.com>
2020-10-15 00:05:56 +0200

    Feature/attiny85 (#11)
    * alternative interrupts for ATtiny85 #10 
    * different LED pin on ATtiny85
    * bugfix microsecond int type #8 
    * workflow action to compile ATtiny85 as well
    * react immediatly to moving wheels
    * updated README.md

--- Controller/Controller.ino
+++ Controller/Controller.ino
@@ -1,24 +1,39 @@
 /*
  * Einlesen eines Näherungssensors für die Speichen eines Wagenrades einer Modelleisenbahn.
- * Wenn sich das Rad bewegt (Zug fährt), soll ein Pin auf HIGH geschaltet werden. Wenn der Sensor
- * eine gewisse Zeit kein Signal liefert (Zug hält), wird der Pin auf LOW geschaltet.
+ * Wenn sich das Rad bewegt (Zug fährt), sollen zwei Pins (für Drehlichter) auf HIGH geschaltet werden.
+ * Wenn der Sensor eine gewisse Zeit kein Signal liefert (Zug hält), wird der Pin auf LOW geschaltet.
  */
 
-#include <TimerOne.h>
+#include "TimerOne.h"
+#if defined(__AVR_ATtiny85__)
+#include "PinChangeInterrupt.h"
+#endif
 
-#define SENSOR_INPUT	2   // Arduino Uno/Nano/Mini: Only Pin 2/3 allowed
-#define SIGNAL_OUTPUT  13   // Test mit LED
+#if defined(__AVR_ATtiny85__)
+#define LED_OUTPUT        1   // LED für ATtiny85
+#else
+#define LED_OUTPUT       13   // LED für andere Arduinos
+#endif
+#define SENSOR_INPUT	  2   // Arduino Uno/Nano/Mini: Nur Pin 2/3 erlaubt, ATtiny85 kann alle Pins benutzen
+#define DREHLICHT_HINTEN  4
+#define DREHLICHT_VORNE   5
 
-#define MILLISECONDS 1000
-#define INTERVAL     1000 * MILLISECONDS
+#define MILLISECONDS   1000L
+#define INTERVAL      (1000 * MILLISECONDS)
 
 int spokeCount = 0;
 
 void setup() {
   Serial.begin(9600);
   pinMode(SENSOR_INPUT, INPUT);
-  pinMode(SIGNAL_OUTPUT, OUTPUT);
-  attachInterrupt(digitalPinToInterrupt(SENSOR_INPUT), signalIsr, CHANGE);
+  pinMode(LED_OUTPUT, OUTPUT);
+  pinMode(DREHLICHT_HINTEN, OUTPUT);
+  pinMode(DREHLICHT_VORNE, OUTPUT);
+  #if defined(__AVR_ATtiny85__)
+    attachPinChangeInterrupt(digitalPinToPinChangeInterrupt(SENSOR_INPUT), signalIsr, CHANGE);
+  #else
+    attachInterrupt(digitalPinToInterrupt(SENSOR_INPUT), signalIsr, CHANGE);
+  #endif
   Timer1.initialize(INTERVAL);
   Timer1.attachInterrupt(timerIsr);
 }
@@ -31,6 +46,7 @@ void setup() {
  */
 void signalIsr(void) {
   spokeCount += 1;
+  trainMoving(true);
 }
 
 /*
@@ -39,16 +55,21 @@ void signalIsr(void) {
  * Zähler wieder auf 0 gesetzt.
  */
 void timerIsr(void) {
-  trainStopped(spokeCount == 0);
+  trainMoving(spokeCount != 0);
   spokeCount = 0;
 }
 
 /*
  * Der Modus des Wagens wird auf "fährt" oder "wartet" gesetzt.
+ * Es werden zwei Pins geschaltet, um genügend Leistung für die Drehlichter zu erzeugen.
  */
-void trainStopped(bool stopped) {
-  digitalWrite(SIGNAL_OUTPUT, stopped ? LOW : HIGH);
-  Serial.println(stopped ? "wartet" : "fährt");
+void trainMoving(bool moving) {
+  #if defined SHOW_LED
+    digitalWrite(LED_OUTPUT, moving ? HIGH : LOW);
+  #endif
+  digitalWrite(DREHLICHT_VORNE, moving ? HIGH : LOW);
+  digitalWrite(DREHLICHT_HINTEN, moving ? HIGH : LOW);
+  Serial.println(moving ? "fährt" : "wartet");
 }
 
 void loop() {
