69097247e7f7ae2637d3e71a9be0324f8ae8bd67
joergkeller <joerg.keller@gmx.ch>
2020-10-14 22:28:54 +0200

    alternative interrupts for attiny85

--- Controller/Controller.ino
+++ Controller/Controller.ino
@@ -4,10 +4,15 @@
  * eine gewisse Zeit kein Signal liefert (Zug hält), wird der Pin auf LOW geschaltet.
  */
 
-#include <TimerOne.h>
+#include "TimerOne.h"
+#if defined(__AVR_ATtiny85__)
+#include "PinChangeInterrupt.h"
+#endif
 
 #define SENSOR_INPUT	2   // Arduino Uno/Nano/Mini: Only Pin 2/3 allowed
-#define SIGNAL_OUTPUT  13   // Test mit LED
+#define LED_OUTPUT     13   // Test mit LED
+#define DREHLICHT_HINTEN  4
+#define DREHLICHT_VORNE   5
 
 #define MILLISECONDS 1000
 #define INTERVAL     1000 * MILLISECONDS
@@ -17,8 +22,16 @@ int spokeCount = 0;
 void setup() {
   Serial.begin(9600);
   pinMode(SENSOR_INPUT, INPUT);
-  pinMode(SIGNAL_OUTPUT, OUTPUT);
-  attachInterrupt(digitalPinToInterrupt(SENSOR_INPUT), signalIsr, CHANGE);
+  #if not defined(__AVR_ATtiny85__)
+    pinMode(LED_OUTPUT, OUTPUT);
+  #endif
+  pinMode(DREHLICHT_HINTEN, OUTPUT);
+  pinMode(DREHLICHT_VORNE, OUTPUT);
+  #if defined(__AVR_ATtiny85__)
+    attachPinChangeInterrupt(digitalPinToPinChangeInterrupt(SENSOR_INPUT), signalIsr, CHANGE);
+  #else
+    attachInterrupt(digitalPinToInterrupt(SENSOR_INPUT), signalIsr, CHANGE);
+  #endif
   Timer1.initialize(INTERVAL);
   Timer1.attachInterrupt(timerIsr);
 }
@@ -47,7 +60,11 @@ void timerIsr(void) {
  * Der Modus des Wagens wird auf "fährt" oder "wartet" gesetzt.
  */
 void trainStopped(bool stopped) {
-  digitalWrite(SIGNAL_OUTPUT, stopped ? LOW : HIGH);
+  #if not defined(__AVR_ATtiny85__)
+    digitalWrite(LED_OUTPUT, stopped ? LOW : HIGH);
+  #endif
+  digitalWrite(DREHLICHT_VORNE, stopped ? LOW : HIGH);
+  digitalWrite(DREHLICHT_HINTEN, stopped ? LOW : HIGH);
   Serial.println(stopped ? "wartet" : "fährt");
 }
 
